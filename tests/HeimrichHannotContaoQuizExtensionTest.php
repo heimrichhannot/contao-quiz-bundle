924eb237c080a2c27451c2686d79a81294c91821
Konstantin Wagner <k.wagner@heimrich-hannot.de>
2018-01-31 16:54:25 +0100

    added tests

---
+++ tests/HeimrichHannotContaoQuizExtensionTest.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * Copyright (c) 2018 Heimrich & Hannot GmbH
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace HeimrichHannot\QuizBundle\Test;
+
+use HeimrichHannot\QuizBundle\DependencyInjection\HeimrichHannotContaoQuizExtension;
+use HeimrichHannot\QuizBundle\HeimrichHannotContaoQuizBundle;
+use PHPUnit\Framework\TestCase;
+
+class HeimrichHannotContaoQuizExtensionTest extends TestCase
+{
+    public function testGetContainerExtension()
+    {
+        $bundle = new HeimrichHannotContaoQuizBundle();
+
+        $this->assertInstanceOf(HeimrichHannotContaoQuizExtension::class, $bundle->getContainerExtension());
+    }
+}
